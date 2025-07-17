1bfd0eeed3d374eebabc1bfe6a11bdda392204b4
amletodellaloggia <amletodellaloggia@gmail.com>
2025-07-17 13:45:30 +0200

    [upd] snack-4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,8 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const mappedAuthors = posts.map((authorNames) => authorNames.author)
+console.log(mappedAuthors)
+
+
